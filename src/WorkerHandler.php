f7cec2890ffcb1ce88a9a705776674fdb9668876
Kreshnik Gashi <kreshnikg3@gmail.com>
2023-09-28 20:16:38 +0200

    wip lambda rds ses

--- src/WorkerHandler.php
+++ src/WorkerHandler.php
@@ -30,26 +30,36 @@ class WorkerHandler extends SqsHandler
             flags: JSON_THROW_ON_ERROR
         );
 
+        echo "Processing message #{$record->getMessageId()}, Body: {$record->getBody()}\n";
+        $this->logger->info("Processing message #{$record->getMessageId()}, Body: {$record->getBody()}");
+
         $invoice = $this->invoiceRepository->find($message["invoiceId"]);
 
+        echo "Sending invoice #{$invoice->getId()} to email\n";
         $this->logger->info("Sending invoice #{$invoice->getId()} to email");
 
         $result = $this->emailService->sendInvoice($invoice);
 
         if ($result["@metadata"]["statusCode"] !== 200) {
+            echo "Error sending email, messageId: {$record->getMessageId()}, Body: {$record->getBody()}\n";
             throw new \RuntimeException("Error sending email, messageId: {$record->getMessageId()}, Body: {$record->getBody()}");
         }
 
+        echo "Success sending invoice #{$invoice->getId()} to email\n";
         $this->logger->info("Success sending invoice #{$invoice->getId()} to email");
     }
 
     public function handleSqs(SqsEvent $event, Context $context): void
     {
+        echo "Fetching messages from SQS\n";
+        $this->logger->info("Fetching messages from SQS\n");
+
         foreach ($event->getRecords() as $record) {
             try {
                 $this->processRecord($record);
             } catch (\Throwable $th) {
                 echo $th->getMessage();
+                $this->logger->error($th->getMessage());
 
                 $this->markAsFailed($record);
             }
